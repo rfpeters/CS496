263dd0c0ccae4c4738fbdc1676d6ba531dfcad06
rfpeters <rfpeters13@gmail.com>
2016-06-22 13:16:25 -0400

    add index template

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,10 +1,15 @@
+import os
 import webapp2
 import datetime
 
+
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		now = datetime.datetime.now()
-		self.response.write(str(now))
+		now = datetime.datetime.now()		
+		template_values = {'time': now}
+		
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.write(template.render(path, template_values)
 		
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
