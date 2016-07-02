7ad982638eb7ab2be015250bd26581f7f5630058
rfpeters <rfpeters13@gmail.com>
2016-07-01 20:40:37 -0400

    update assignment2

--- assignment2/index.py
+++ assignment2/index.py
@@ -3,4 +3,6 @@ import base_page
 from google.appengine.ext import ndb
 import db_defs
 
-class Index(base_page.BaseHandler)
+class Index(base_page.BaseHandler):
+	def get(self):
+		self.render('index.html')
