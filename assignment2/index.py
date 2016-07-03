0772964b71a0e238ca3ce2ac5c7f21358f3dbccf
rfpeters <rfpeters13@gmail.com>
2016-07-03 16:40:32 -0400

    update

--- assignment2/index.py
+++ assignment2/index.py
@@ -33,31 +33,19 @@ class Index(base_page.BaseHandler):
 			cust.phone_type = self.request.get('phone_type')
 			cust.sports = [ndb.Key(urlsafe=x) for x in self.request.get_all('sports[]')]
 			cust.put()
-			self.render('index.html')
 		elif action == 'add_sport':
 			k = ndb.Key(db_defs.Sports, self.app.config.get('default-group'))
 			sport = db_defs.Sports(parent=k)
 			sport.name = self.request.get('sport_input')
 			sport.put()
-			self.render('index.html')
 		elif action == 'edit_customer':
 			cust_key = ndb.Key(urlsafe=self.request.get('key'))
 			cust = cust_key.get()
-			self.template_values['cust_name'] = cust.name
-			self.template_values['cust_email'] = cust.email
-			self.template_values['cust_phone'] = cust.phone
-			self.template_values['cust_pwd'] = cust.password
-			sports = db_defs.Sports.query(ancestor=ndb.Key(db_defs.Sports, self.app.config.get('default-group'))).fetch()
-			sport_box = []
-			for p in self.phone_types:
-				if p['name'] == cust.phone_type:
-					p['checked'] = True
-			for s in sports:
-				if s.key in cust.sports:
-					sport_box.append({'name':s.name, 'key':s.key.urlsafe(), 'checked':True})
-				else:
-					sport_box.append({'name':s.name, 'key':s.key.urlsafe(), 'checked':False})
-			self.template_values['sports'] = sport_box
-			self.template_values['phone_type'] = self.phone_types
-			base_page.BaseHandler.render(self, 'index.html', self.template_values)
-		
+			cust.name = self.request.get('name_input')
+			cust.email = self.request.get('email_input')
+			cust.password = self.request.get('password_input')
+			cust.phone = self.request.get('phone_input')
+			cust.phone_type = self.request.get('phone_type')
+			cust.sports = [ndb.Key(urlsafe=x) for x in self.request.get_all('sports[]')]
+			cust.put()
+		self.render('index.html')
