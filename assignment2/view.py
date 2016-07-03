d87bc577fed2e65eee1673df66ba3279884a98f9
rfpeters <rfpeters13@gmail.com>
2016-07-03 13:38:49 -0400

    update

--- assignment2/view.py
+++ assignment2/view.py
@@ -16,6 +16,11 @@ class View(base_page.BaseHandler):
 		self.template_values['password'] = customer.password
 		self.template_values['phone'] = customer.phone
 		self.template_values['phone_type'] = customer.phone_type
-		self.template_values['sports'] = customer.sports
+		sports = db_defs.Sports.query(ancestor=ndb.Key(db_defs.Sports, self.app.config.get('default-group'))).fetch()
+		sport = []
+		for s in sports:
+			if s.key in customer.sports:
+				sport.append({'name':s.name})
+		self.template_values['sports'] = sport
 		self.template_values['key'] = self.request.get('key')
 		self.render('view.html', self.template_values)
