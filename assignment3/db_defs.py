4cd5196eec96ead828cf29d2ae8560a5ed4a69d3
rfpeters <rfpeters13@gmail.com>
2016-07-14 23:52:08 -0400

    update assignment3

--- assignment3/db_defs.py
+++ assignment3/db_defs.py
@@ -14,7 +14,12 @@ class Dog(ndb.Model):
 	name = ndb.StringProperty(required=True)
 	breed = ndb.StringProperty(required=True)
 	age = ndb.StringProperty(required=True)
-	arrival = ndb.DateProperty(auto_now_add=True)
+	arrival = ndb.DateProperty(auto_now_add=True
+	
+	def to_dict(self):
+		d = super(Dog, self).to_dict()
+		d['arrival'] = d.arrival.strftime('%m/%d/%Y')
+		return d
 	
 class Cat(ndb.Model):
 	name = ndb.StringProperty(required=True)
