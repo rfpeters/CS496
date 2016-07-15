ae11d31ef83afb932558094a1ed1604b98523643
rfpeters <rfpeters13@gmail.com>
2016-07-14 22:53:53 -0400

    update assignment3

--- assignment3/main.py
+++ assignment3/main.py
@@ -2,5 +2,5 @@ import webapp2
 
 app = webapp2.WSGIApplication([], debug=True)
 app.router.add(webapp2.Route(r'/shelter', 'shelter.Shelter'))
-app.router.add(webapp2.Route(r'/shelter/<id:[0-9]>', 'shelter.Shelter'))
-app.router.add(webapp2.Route(r'/cat/<id:[0-9]>', 'cat.Cat'))
+app.router.add(webapp2.Route(r'/shelter/<id:[0-9]+>', 'shelter.Shelter'))
+app.router.add(webapp2.Route(r'/cat/<id:[0-9]+>', 'cat.Cat'))
