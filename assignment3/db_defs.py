5c3efdf0394940dcb5132b9c7384e1eebb2cdabf
rfpeters <rfpeters13@gmail.com>
2016-07-14 22:31:08 -0400

    update assignment3

--- assignment3/db_defs.py
+++ assignment3/db_defs.py
@@ -1,12 +1,12 @@
 from google.appengine.ext import ndb
 
 class Shelter(ndb.Model):
-	name = ndb.StringProperty()
-	phone = ndb.StringProperty()
-	address = ndb.StringProperty()
-	city = ndb.StringProperty()
-	state = ndb.StringProperty()
-	zip = ndb.KeyProperty()
+	name = ndb.StringProperty(required=True)
+	phone = ndb.StringProperty(required=True)
+	address = ndb.StringProperty(required=True)
+	city = ndb.StringProperty(required=True)
+	state = ndb.StringProperty(required=True)
+	zip = ndb.KeyProperty(required=True)
 	
 class Dog(ndb.Model):
 	name = ndb.StringProperty(required=True)
