484fe3160893a045263db32077799bcd097f1f9c
rfpeters <rfpeters13@gmail.com>
2016-07-15 22:47:06 -0400

    update assignment3

--- assignment3/db_defs.py
+++ assignment3/db_defs.py
@@ -25,7 +25,8 @@ class Dog(Model):
 		d = super(Dog, self).to_dict()
 		d['arrival'] = self.arrival.strftime('%m/%d/%Y')
 		if self.shelter:
-			d['shelter'] = self.shelter.id()
+			d['shelter'] = 1
+			#d['shelter'] = self.shelter.id()
 		return d
 	
 class Cat(ndb.Model):
