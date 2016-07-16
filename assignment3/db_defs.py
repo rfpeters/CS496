8a3c9fe7acdbbbe251b9a749a3497bc2bd3fbfee
rfpeters <rfpeters13@gmail.com>
2016-07-15 22:32:49 -0400

    update assignment3

--- assignment3/db_defs.py
+++ assignment3/db_defs.py
@@ -24,7 +24,7 @@ class Dog(Model):
 	def to_dict(self):
 		d = super(Dog, self).to_dict()
 		d['arrival'] = self.arrival.strftime('%m/%d/%Y')
-		if d['shelter']:
+		if d['shelter'] is not None:
 			d['shelter'] = self.shelter.id()
 		return d
 	
